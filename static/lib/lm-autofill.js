78bc1f022a60fd0609c710a45486f228b08b6619
gasoved <gasoved@gmail.com>
2021-08-04 04:52:39 +0300

    feat: tolerate single spaces in mentions

--- static/lib/lm-autofill.js
+++ static/lib/lm-autofill.js
@@ -7,9 +7,19 @@ $(document).ready(function () {
 
 	$(window).on('composer:autocomplete:init', function (ev, data) {
 		let slugify;
+		let prevTerm = '';
+		let noMatch = false;
+
 		const strategy = {
-			match: new RegExp(`\\B\\${SYMBOL}([^\\s\\n]*)?$`),
+			match: new RegExp(`\\B\\${SYMBOL}(([^\\s\\n]\\s?)*)?$`),
 			search: function (term, callback) {
+				// Don't search on further key presses if there's no match already
+				if (term && term.startsWith(prevTerm) && noMatch) {
+					return callback([]);
+				}
+				noMatch = false;
+				prevTerm = term;
+
 				// Get composer metadata
 				const uuid = data.options.className && data.options.className.match(/dropdown-(.+?)\s/)[1];
 				require(['composer', 'slugify'], function (composer, _slugify) {
@@ -22,6 +32,7 @@ $(document).ready(function () {
 							return callback([]);
 						}
 
+						noMatch = pairs.length < 1;
 						callback(pairs.sort((a, b) => a.toLocaleLowerCase() > b.toLocaleLowerCase()));
 					});
 				});
